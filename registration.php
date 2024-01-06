2afdbfbb73ee0bc2b14654e8d61d16fbbe5c8333
serkanakyol <serkan.akyol@venhancer.com>
2024-01-06 18:49:22 +0300

    initial commit

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * *
+ *  * @author VxCommerce
+ *  * @copyright Copyright (c) 2024 VxCommerce (https://magento2.vxcommerce.net)
+ *  * @package Magento Core Package
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'VxCommerce_Core',
+    __DIR__
+);
